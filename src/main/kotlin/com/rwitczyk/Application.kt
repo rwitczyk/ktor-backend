668b46fcd7dc8de090e0b82c7952150e6714981f
Robert Witczyk <rwitczyk@wp.pl>
2021-05-05 21:44:04 +0200

    init commit

---
+++ src/main/kotlin/com/rwitczyk/Application.kt
@@ -0,0 +1,12 @@
+package com.rwitczyk
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.rwitczyk.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSerialization()
+    }.start(wait = true)
+}

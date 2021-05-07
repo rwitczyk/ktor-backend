fdba9f0676bf78132eb357da6c87eab9a9be1d2f
Robert Witczyk <rwitczyk@wp.pl>
2021-05-08 00:46:19 +0200

    Dodane corsy

--- src/main/kotlin/com/rwitczyk/Application.kt
+++ src/main/kotlin/com/rwitczyk/Application.kt
@@ -5,13 +5,28 @@ import io.ktor.server.netty.*
 import com.rwitczyk.plugins.*
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
 import org.jetbrains.exposed.sql.Database
+import java.time.Duration
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
         configureSerialization()
         initDB()
+
+        install(CORS)
+        {
+            method(HttpMethod.Options)
+            header(HttpHeaders.XForwardedProto)
+            anyHost()
+
+            allowCredentials = true
+            allowNonSimpleContentTypes = true
+            maxAge = Duration.ofDays(1)
+        }
     }.start(wait = true)
 }
 
